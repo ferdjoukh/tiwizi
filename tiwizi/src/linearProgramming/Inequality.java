9d33b1cf51ebd0bead6e31adb696b0dc51a410dc
ferdjoukh-adel <ferdjoukh@lirmm.fr>
2018-04-11 11:43:26 +0200

    Inequality Class tested

---
+++ tiwizi/src/linearProgramming/Inequality.java
@@ -0,0 +1,87 @@
+package linearProgramming;
+
+/**
+ *
+ * @author Adel ferdjoukh
+ * @email ferdjoukh[at]gmail[dot]com
+ *
+ * This class defines an Inequality for TIWIZI tool.
+ *   The inequalities that are defined here have the following shape:
+ *   	1*x +2*y -3*z <= 0
+ *   
+ *   	x,y,z are unknowns
+ *      1,2,-3 are coefficient
+ *      <= is the operand
+ *      
+ *      
+ */
+public class Inequality {
+	
+	private int MAX=10;
+	
+	private String [] unknowns;
+	private int [] coefficients;
+	private String operand;
+	private int position;
+	 
+	/**
+	 * It creates an empty inequality by giving only the operand 
+	 * 
+	 * @param operand <=
+	 */
+	public Inequality(String operand){
+		this.operand=operand;
+		unknowns= new String[MAX];
+		coefficients = new int[MAX];
+		position=0;
+	}
+	
+	/**
+	 * 
+	 * It adds a new variable to the inequality
+	 *  
+	 * @param unknown x,y,...
+	 * @param coeff 0,1,2,-1,....
+	 */
+	public void addVariable(String unknown,int coeff){
+		unknowns[position]=unknown;
+		coefficients[position]=coeff;
+		position++;
+	}
+
+	public String[] getUnknowns() {
+		return unknowns;
+	}
+
+	public int[] getCoefficients() {
+		return coefficients;
+	}
+
+	public String getOperand() {
+		return operand;
+	}
+	
+	/**
+	 * It prints an equation unsing the classical shape: a*x +b*y -c*z <= 0
+	 */
+	public String toString(){
+		
+		String result="";
+		
+		for(int i=0;i<position;i++){
+			if(i==0){
+					result= result+ coefficients[i]+"*"+unknowns[i];
+			}else{
+				if(coefficients[i]<0)
+					result= result+ " "+ coefficients[i]+"*"+unknowns[i];
+				else
+					result= result+ " +"+ coefficients[i]+"*"+unknowns[i];
+			}
+		}
+		
+		result= result +" "+ operand+ " 0";
+		
+		return result;
+	}
+
+}
---
+++ tiwizi/src/tests/InequalityTests.java
@@ -0,0 +1,57 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import linearProgramming.Inequality;
+
+public class InequalityTests {
+
+	@Test
+	public void createEmptyEquation() {
+		Inequality equation= new Inequality("<=");
+		assertEquals("Empty equation", " <= 0", equation.toString());
+	}
+	
+	@Test
+	public void CreateOneUnknownEquation(){
+		Inequality equation = new Inequality("<=");
+		equation.addVariable("Room", 2);
+		assertEquals("2*Room <= 0", equation.toString());
+	}
+	
+	@Test 
+	public void CreateTwoVariablePositiveNegative(){
+		Inequality equation = new Inequality("<=");
+		equation.addVariable("House", 0);
+		equation.addVariable("Room", -1);
+		assertEquals("0*House -1*Room <= 0", equation.toString());
+	}
+	
+	@Test 
+	public void CreateTwoVariablePositivePositive(){
+		Inequality equation = new Inequality("<=");
+		equation.addVariable("House", 1);
+		equation.addVariable("Room", 1);
+		assertEquals("1*House +1*Room <= 0", equation.toString());
+	}
+	
+	@Test 
+	public void CreateTwoVariableNegativeNegative(){
+		Inequality equation = new Inequality("<=");
+		equation.addVariable("House", -1);
+		equation.addVariable("Room", -1);
+		assertEquals("-1*House -1*Room <= 0", equation.toString());
+	}
+	
+	@Test 
+	public void CreateThreeVariables(){
+		Inequality equation = new Inequality("<=");
+		equation.addVariable("Room1", 1);
+		equation.addVariable("Room2", 1);
+		equation.addVariable("House", -2);
+		assertEquals("1*Room1 +1*Room2 -2*House <= 0", equation.toString());
+	}
+
+}
