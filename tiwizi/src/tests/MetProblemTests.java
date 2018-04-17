4b954275f346b1b0ada4a790c7e5cfec09c2ab9d
ferdjoukh-adel <ferdjoukh@lirmm.fr>
2018-04-17 15:16:55 +0200

    MetProblem Class is tested

--- tiwizi/src/tests/MetProblemTests.java
+++ tiwizi/src/tests/MetProblemTests.java
@@ -1,5 +1,9 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.*;
 import linearProgramming.*;
 import utils.MetProblem;
@@ -8,10 +12,74 @@ public class MetProblemTests {
 
 	@Test
 	public void createSingleTest(){
-		
 				
-		MetProblem problem= new MetProblem(inequality, candidates);
+		Inequality equation = new Inequality("<=");
+		equation.addVariable("Wall", 1);
+		equation.addVariable("Room", -4);
 		
+		ArrayList<Integer> candidate= new ArrayList<Integer>();
+		candidate.add(5);
+		candidate.add(1);
+		
+		MetProblem problem= new MetProblem(equation,candidate);
+		
+		System.out.println(problem.toString());
+		
+		assertEquals("More [Room] or Less [Wall]",problem.getMessage());	
+	}
+	
+	@Test
+	public void createSingleTest2(){
+				
+		Inequality equation = new Inequality("<");
+		equation.addVariable("Wall", 1);
+		equation.addVariable("Room", -4);
+		
+		ArrayList<Integer> candidate= new ArrayList<Integer>();
+		candidate.add(5);
+		candidate.add(1);
+		
+		MetProblem problem= new MetProblem(equation,candidate);
+		
+		System.out.println(problem.toString());
+		
+		assertEquals("More [Room] or Less [Wall]",problem.getMessage());	
+	}
+	
+	@Test
+	public void createSingleTest3(){
+				
+		Inequality equation = new Inequality(">=");
+		equation.addVariable("Wall", 1);
+		equation.addVariable("Room", -4);
+		
+		ArrayList<Integer> candidate= new ArrayList<Integer>();
+		candidate.add(5);
+		candidate.add(1);
+		
+		MetProblem problem= new MetProblem(equation,candidate);
+		
+		System.out.println(problem.toString());
+		
+		assertEquals("More [Wall] or Less [Room]",problem.getMessage());	
+	}
+
+	@Test
+	public void createSingleTest4(){
+				
+		Inequality equation = new Inequality(">");
+		equation.addVariable("Wall", 1);
+		equation.addVariable("Room", -4);
+		
+		ArrayList<Integer> candidate= new ArrayList<Integer>();
+		candidate.add(5);
+		candidate.add(1);
+		
+		MetProblem problem= new MetProblem(equation,candidate);
+		
+		System.out.println(problem.toString());
+		
+		assertEquals("More [Wall] or Less [Room]",problem.getMessage());	
 	}
 	
 }
--- tiwizi/src/utils/MetProblem.java
+++ tiwizi/src/utils/MetProblem.java
@@ -19,6 +19,7 @@ public class MetProblem {
 	public MetProblem(Inequality inequality, ArrayList<Integer> candidates){
 		this.inequality=inequality;
 		this.candidates=candidates;
+		setMessage();
 	}
 	
 	/**
@@ -31,32 +32,51 @@ public class MetProblem {
 	 */
 	public void setMessage(){
 		
+		ArrayList<String> plus= new ArrayList<String>();
+		ArrayList<String> moins= new ArrayList<String>();
 		
+		for(int i=0;i<inequality.getPosition();i++){
+			if(inequality.getCoefficients()[i]>0){
+				plus.add(inequality.getUnknowns()[i]);
+			}
+			else if(inequality.getCoefficients()[i]<0){
+				moins.add(inequality.getUnknowns()[i]);
+			}
+		}
 		
-		this.message="";
+		String more="More ";
+		String less="Less ";
+		
+		switch(inequality.getOperand()){
+			case "<=": case "<":
+				more=more+ moins.toString();
+				less=less+ plus.toString();
+				break;
+			
+			case ">=": case ">":
+				more=more+ plus.toString();
+				less=less+ moins.toString();
+				break;
+		}
+		
+		this.message= more+ " or "+ less;
 	}
 	
-	public MetProblem(){
-		
-	}
-			
 	public Inequality getInequality() {
 		return inequality;
-	}
-
-	public void setInequality(Inequality inequality) {
-		this.inequality = inequality;
 	}
 
 	public ArrayList<Integer> getCandidates() {
 		return candidates;
 	}
 
-	public void setCandidates(ArrayList<Integer> candidates) {
-		this.candidates = candidates;
-	}
-
 	public String getMessage() {
 		return message;
+	}
+	
+	public String toString(){
+		
+		String res= inequality.toString()+ " " +candidates.toString()+ " >> " + message;
+		return res;
 	}
 }
